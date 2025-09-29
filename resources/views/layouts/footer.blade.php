f3126d566154f4685d9f921043602aa7c884bbfb
Kaisodesu <alrickjelotugano@gmail.com>
2025-09-29 12:33:35 +0800

    Updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Mamilot's Inventory Management System &copy;2024</span>
+            <span>Mamilot's Inventory Management System &copy;2025</span>
         </div>
     </div>
 </footer>
